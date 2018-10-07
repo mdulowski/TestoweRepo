ab68abaec5abdabd9e02244e14ca7c47a4911543
MD <marcin.dulowski@PK3-4>
2018-10-07 14:46:41 +0200

    drugi testowy Comit

---
+++ Database1/Database1/dbo/Views/vDimStore.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vDimStore]
+AS
+SELECT 
+[StoreKey],
+[StoreType],
+[StoreName],
+[StoreDescription],
+[Status],
+[nowa_kolumna] = 1 
+FROM[dbo].[DimStore]
---
+++ Database1/Database1/testowy/Tables/tabela1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [testowy].[tabela1] (
+    [kolumna1] INT            NOT NULL,
+    [kolumna2] NVARCHAR (100) DEFAULT (N'wartość tekstowa') NOT NULL,
+    PRIMARY KEY CLUSTERED ([kolumna1] ASC)
+);
+
